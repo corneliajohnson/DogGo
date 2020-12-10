7a6f81007263d0212a5807026d878325836e5185
corneliajohnson <corneliaj91@gmail.com>
2020-12-10 16:00:19 -0500

    added owner model

---
+++ DogGo/Repositories/OwnerRepository.cs
@@ -0,0 +1,28 @@
+﻿using DogGo.Models;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using System.Collections.Generic;
+
+namespace DogGo.Repositories
+{
+    public class OwnerRepository : IOwnerRepository
+    {
+        private readonly IConfiguration _config;
+
+        public OwnerRepository(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        public List<Owner> GetAllOwners()
+        {
+
+        }
+
+        public Owner GetWalkerById(int id)
+        {
+
+        }
+    }
+
+}
