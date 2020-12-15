d801d22afdad9f099d7364eecfa023218648c5fd
corneliajohnson <corneliaj91@gmail.com>
2020-12-15 15:13:41 -0500

    add a owner drop down

--- DogGo/Controllers/DogController.cs
+++ DogGo/Controllers/DogController.cs
@@ -1,4 +1,5 @@
 ﻿using DogGo.Models;
+using DogGo.Models.ViewModels;
 using DogGo.Repositories;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -10,10 +11,12 @@ namespace DogGo.Controllers
     public class DogController : Controller
     {
         private readonly IDogRepository _dogRepository;
+        private readonly IOwnerRepository _ownerRepository;
 
-        public DogController(IDogRepository dogRepository)
+        public DogController(IDogRepository dogRepository, IOwnerRepository ownerRepository)
         {
             _dogRepository = dogRepository;
+            _ownerRepository = ownerRepository;
         }
 
         // GET: DogController
@@ -38,22 +41,29 @@ namespace DogGo.Controllers
         // GET: DogController/Create
         public ActionResult Create()
         {
-            return View();
+            List<Owner> owners = _ownerRepository.GetAllOwners();
+
+            DogFormViewModel vm = new DogFormViewModel()
+            {
+                Dog = new Dog(),
+                Owners = owners,
+            };
+            return View(vm);
         }
 
         // POST: DogController/Create
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create(Dog dog)
+        public ActionResult Create(DogFormViewModel vm)
         {
             try
             {
-                _dogRepository.AddDog(dog);
+                _dogRepository.AddDog(vm.Dog);
                 return RedirectToAction(nameof(Index));
             }
-            catch
+            catch (Exception ex)
             {
-                return View(dog);
+                return View(vm);
             }
         }
 
@@ -61,28 +71,35 @@ namespace DogGo.Controllers
         public ActionResult Edit(int id)
         {
             Dog dog = _dogRepository.GetDogById(id);
+            List<Owner> owners = _ownerRepository.GetAllOwners();
+
+            DogFormViewModel vm = new DogFormViewModel()
+            {
+                Dog = dog,
+                Owners = owners
+            };
 
             if (dog == null)
             {
                 return NotFound();
             }
 
-            return View(dog);
+            return View(vm);
         }
 
         // POST: DogController/Edit/5
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, Dog dog)
+        public ActionResult Edit(int id, DogFormViewModel vm)
         {
             try
             {
-                _dogRepository.UpdateDog(dog);
+                _dogRepository.UpdateDog(vm.Dog);
                 return RedirectToAction(nameof(Index));
             }
             catch
             {
-                return View(dog);
+                return View(vm);
             }
         }
 
